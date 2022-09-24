861c8bebd0d86705b9b5248ffa42f7e25f655202
emonibnsalim <emonibnsali@gmail.com>
2022-09-24 16:05:54 +0600

    last updated

---
+++ GameBoard.js
@@ -0,0 +1,112 @@
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
+
+const WINING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
+const winningMessageElement = document.getElementById("winningMessage");
+const restartButton = document.getElementById("restartButton");
+let circleTurn;
+
+startGame();
+
+restartButton.addEventListener("click", startGame);
+
+function startGame() {
+  circleTurn = false;
+  cellElements.forEach((cell) => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(CIRCLE_CLASS);
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHoverClass();
+  winningMessageElement.classList.remove("show");
+}
+
+function handleClick(e) {
+
+  const cell = e.target;
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+
+  //check for win
+  if (checkWin(currentClass)) {
+    // console.log(checkWin(currentClass));
+    endGame(false);
+  } else if (isDraw()) {
+    //check for draw
+    endGame(true);
+  } else {
+    //switch turns
+    swapTurns();
+    setBoardHoverClass();
+  }
+}
+
+// console.log(circleTurn);
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = "Draw";
+  } else {
+    // console.log(circleTurn);
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+  }
+  winningMessageElement.classList.add("show");
+}
+
+function isDraw() {
+  return [...cellElements].every((cell) => {
+    return (
+      cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    );
+  });
+}
+
+//placeMark
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+//swapTurns
+function swapTurns() {
+  circleTurn = !circleTurn;
+}
+
+// setBoardHoverClass
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS);
+  board.classList.remove(CIRCLE_CLASS);
+
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS);
+  } else {
+    board.classList.add(X_CLASS);
+  }
+}
+cellElements;
+function checkWin(currentClass) {
+  return WINING_COMBINATIONS.some((combinations) => {
+    console.log(combinations);
+    return combinations.every((index) => {
+      // console.log(index);
+      // console.log(cellElements[index]);
+      console.log(currentClass);
+      // console.log(cellElements[index].classList.contains(currentClass));
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
+}
